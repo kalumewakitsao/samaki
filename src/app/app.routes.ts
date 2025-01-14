7216b468697a70388e92d7ae0545408242244543
Kalume Kitsao <kalumekitsao@MiKaKis-Macbook-Pro-M1.local>
2025-01-14 21:19:35 +0300

    add necessary pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-      },
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'contacts',
+        component: ContactsComponent,
+    },
 ];
